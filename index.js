dc78385ccfe7d668d1159cbb0f1987f41f3d6a45
Lukasz Piotrowski <lpiotrowski@op.pl>
2019-02-01 18:24:02 +0100

    Fix const and move function file to outside

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-let http = require('http');
-let fs = require('fs');
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer();
 
-let server = http.createServer();
+function file(response, file, type) {
+    fs.readFile('./' + file, function (err, data) {
+        if (!err) {
+            response.setHeader("Content-Type", type + '; charset=utf-8');
+            response.write(data);
+            response.end();
+        } else {
+            response.statusCode = 404;
+            response.setHeader("Content-Type", type + '; charset=utf-8');
+            response.end();
+        }
+    });
+}
 
 server.on('request', function (request, response) {
-    function file(file, type) {
-        fs.readFile('./' + file, function (err, data) {
-            if (!err) {
-                response.setHeader("Content-Type", type + '; charset=utf-8');
-                response.write(data);
-                response.end();
-            } else {
-                response.statusCode  = 404;
-                response.setHeader("Content-Type", type + '; charset=utf-8');
-                response.end();
-            }
-        });
-    }
+
     if (request.method === 'GET') {
         switch (request.url) {
-            case '/style.css': file('style.css', 'text/css');
+            case '/style.css': file(response, 'style.css', 'text/css');
                 break;
-            case '/': if (request.method === 'GET') file('index.html', 'text/html');
+            case '/': file(response, 'index.html', 'text/html');
                 break;
-            default: file('error.jpg', 'image/jpg');
+            default: file(response, 'error.jpg', 'image/jpg');
         }
     }
 });
